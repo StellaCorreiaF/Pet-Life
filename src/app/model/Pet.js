a12b71b2aed14dd01ea2789b41172fe1b5beb371
StellaCorreiaF <stellafernanda@icloud.com>
2022-07-18 19:21:25 -0300

    corrige atributo da função

--- src/app/model/Pet.js
+++ src/app/model/Pet.js
@@ -1,8 +1,8 @@
-function petModel(id, nome, peso, tipoSanguinio, raca, idade, endereco){  
+function petModel(id, nome, peso, tipoSanguineo, raca, idade, endereco){  
     this.id = id; 
     this.nome = nome; 
     this.peso = peso; 
-    this.tipoSanguinio = tipoSanguinio; 
+    this.tipoSanguineo = tipoSanguineo; 
     this.raca = raca; 
     this.idade = idade; 
     this.endereco = endereco;
