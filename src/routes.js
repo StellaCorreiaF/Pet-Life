ca53a0b58b0171397ddad878d6d46108181b71a7
StellaCorreiaF <stellafernanda@icloud.com>
2022-07-18 22:16:39 -0300

    cria branch para manipulação de rotas do projeto

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ const Router = require("express");
 const controller = require('./app/controller/PetController') 
 
 const routes = new Router(); 
+// ROTAS PETS
+routes.get('/pets', controller.index)
 
-routes.get('/pets', controller.index) 
+//ROTAS TUTORES
+
+//ROTAS VETERINARIOS
+
 
 module.exports = routes;  
 
