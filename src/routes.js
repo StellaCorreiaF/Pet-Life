8e754487b652564330d96303aadd51c8bee8bb90
StellaCorreiaF <stellafernanda@icloud.com>
2022-07-20 20:53:46 -0300

    adiciona rota POST para cadastrar veterinario

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const Router = require("express"); 
-const controller = require('./app/controller/PetController') 
+const controller = require('./app/controller/PetController');
+const vetController = require('./app/controller/VetController'); 
 
 const routes = new Router(); 
 
 routes.get('/pets', controller.index) 
 
+
+// ROTAS VET
+routes.post('/vets', vetController.create)
 module.exports = routes;  
 
 
