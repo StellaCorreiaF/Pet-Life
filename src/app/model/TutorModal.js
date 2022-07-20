d1ff354f15b380ab3daf51e16370166657f847aa
Lilian Melo <107283230+LilianLucena@users.noreply.github.com>
2022-07-19 21:35:24 -0300

    Add files via upload

---
+++ src/app/model/TutorModal.js
@@ -0,0 +1,11 @@
+function tutorModel(id, nome, email, username, senha, telefone){  
+    this.id = id; 
+    this.nome = nome; 
+    this.email = email; 
+    this.username = username; 
+    this.senha = senha; 
+    this.telefone = telefone; 
+   
+} 
+
+module.exports = tutorModel;
