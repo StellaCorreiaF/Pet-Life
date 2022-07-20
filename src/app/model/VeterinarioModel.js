6e1256dcf9bdac788c29eadb49be0bdca59ae5a3
StellaCorreiaF <stellafernanda@icloud.com>
2022-07-20 20:54:01 -0300

    ajusta nome da classe

---
+++ src/app/model/VeterinarioModel.js
@@ -0,0 +1,10 @@
+function VeterinarioModel(id, nome, crmv, telefone, email, senha) {
+  this.id = id;
+  this.nome = nome;
+  this.crmv = crmv;
+  this.telefone = telefone;
+  this.email = email;
+  this.senha = senha;
+}
+
+module.exports = VeterinarioModel;
