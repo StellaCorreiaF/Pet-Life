e2703f11f77060d8c55aa350c08fbcb2a7792420
lariarauj0 <laris.c.araujo@gmail.com>
2022-07-21 16:56:33 -0300

    modificando routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 const Router = require("express"); 
 const controller = require('./app/controller/PetController') 
 
+const controllerPET = require('./app/controller/PET/PetController')
+
 const routes = new Router(); 
+
+
 // ROTAS PETS
-routes.get('/pets', controller.index)
+//routes.get('/pets', controller.index)
+
+routes.post("/pets", controllerPET.create);
+routes.put("/pets/:id", controllerPET.update);
 
 //ROTAS TUTORES
 
 //ROTAS VETERINARIOS
-routes.post('veterinarioS', vetController.create)
+//routes.post('veterinarioS', vetController.create)
 
 module.exports = routes;  
 
