1b8e9e6a468ebfbf62038588ffad74a9494f65d2
StellaCorreiaF <stellafernanda@icloud.com>
2022-07-25 20:28:43 -0300

    adiciona rotas

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,12 @@
 
 const Router = require("express"); 
-//const controller = require('./app/controller/PetController') 
-
-const controllerPET = require('./app/controller/PET/PetController')
-
-
+const controllerPET = require('./app/controller/PET/PetController'); 
+const vetController = require("./app/controller/VetController");
 const routes = new Router();
 
 
 
 // ROTAS PETS
-//routes.get('/pets', controller.index) 
 // routes.get('/pets', controllerPET.index)
 routes.get("/pets", controllerPET.listData)
 routes.post("/pets", controllerPET.create);
@@ -18,12 +14,6 @@ routes.put("/pets/:id", controllerPET.update);
 routes.delete('/pets/:id', controllerPET.delete)
 
 //ROTAS TUTORES
-
-//ROTAS VETERINARIOS
-//routes.post('veterinarioS', vetController.create)
-
-module.exports = routes;  
-
 
 // ROTAS VET
 routes.get("/vets", vetController.listAll);
