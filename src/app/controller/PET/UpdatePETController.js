03a56a38ef1d0a22cf7a06d161755e37742237ba
lariarauj0 <laris.c.araujo@gmail.com>
2022-09-06 16:02:19 -0300

    criando arquivo UpdatePETController.js

---
+++ src/app/controller/PET/UpdatePETController.js
@@ -0,0 +1,31 @@
+import UpdatePetService from "../../service/PET/UpdatePetService";
+
+export default class UpdatePETController {
+    constructor() {
+      this.service = new UpdatePetService();
+    }
+  
+    async update(request, response) {
+      const { id } = request.params;
+      const {
+          nome,
+          peso,
+          tipoSanguineo,
+          raca,
+          idade
+      } = request.body;
+  
+  
+      const updatedPet= await this.service.update(
+          id,
+          nome,
+          peso,
+          tipoSanguineo,
+          raca,
+          idade
+      )
+      
+      return response.status(200).json(updatedPet)     
+    }
+
+    }
