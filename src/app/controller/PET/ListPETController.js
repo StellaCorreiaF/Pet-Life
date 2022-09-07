a6c82376115c0d0245ba51cff34cfcf46319ac29
baacelar <heriton.alves@ufba.br>
2022-09-07 12:55:05 -0300

    Subindo atualização do list e delete

---
+++ src/app/controller/PET/ListPETController.js
@@ -0,0 +1,16 @@
+import ListPetService from "../../service/PET/ListPetService"; 
+
+export default class ListPetcontroller{ 
+    constructor(){ 
+        this.service = new ListPetService();
+    } 
+
+    async index(request, response){ 
+        const {name} = request.query; 
+
+        const pets = await this.service.listAll(name); 
+
+        response.json(pets)
+    }
+} 
+
--- src/app/service/PET/DeletePetService.js
+++ src/app/service/PET/DeletePetService.js
@@ -1,17 +1,42 @@
-const ListPetService = require('../../service/PET/ListPetService');
+import petModel from "../../model/Pet"; 
 
-const DeletePetService = { 
-    delete: (id) => { 
-        const pets = ListPetService.listPetServ() 
-        const petIndice = pets.findIndex(item => item.id === Number(id)) 
+export default class DeletePetService{ 
+    constructor(){} 
 
-        if (petIndice < 0){ 
-            return {erro: 'Pet não encontrado'}
-        } 
-        
-        pets.splice(petIndice, 1) 
-        return {mensagem: 'Cadastro removido com sucesso'}
+    async delete(id){ 
+        try{ 
+            const pet = await petModel.findByPk(id); 
+
+            if(!pet){ 
+                return { mensagem: "Pet não ecnocntrado"};
+            } 
+
+            const petRemovido = await pet.destroy(); 
+            return petRemovido
+        } catch(error){ 
+            console.log(error); 
+            return {erro: error.message};
+        }
     }
 }
 
-module.exports = DeletePetService
+
+
+
+// const ListPetService = require('../../service/PET/ListPetService');
+
+// const DeletePetService = { 
+//     delete: (id) => { 
+//         const pets = ListPetService.listPetServ() 
+//         const petIndice = pets.findIndex(item => item.id === Number(id)) 
+
+//         if (petIndice < 0){ 
+//             return {erro: 'Pet não encontrado'}
+//         } 
+        
+//         pets.splice(petIndice, 1) 
+//         return {mensagem: 'Cadastro removido com sucesso'}
+//     }
+// }
+
+// module.exports = DeletePetService
