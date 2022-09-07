2b4198da7e1ed9623fa5209f20a2fda0ad64fe55
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-07 11:37:46 -0300

    cria serviço para criação de vet

--- src/app/service/VET/CreateVeterinarioService.js
+++ src/app/service/VET/CreateVeterinarioService.js
@@ -1,28 +1,39 @@
-import veterinario from "../../model/VeterinarioModel";
 import { v4 } from "uuid";
 import { create } from "yup/lib/Reference";
+import VeterinarioModel from "../../model/VeterinarioModel";
 
-const createVeterinarioService = {
-  createVeterinario: (
-    nome,
-    telefone,
-    email,
-    login,
-    senha,
-    crmv,
-    especialidade
-  ) => {
-    const newVeterinario = new veterinario(
-      v4(),
-      nome,
-      telefone,
-      email,
-      login,
-      senha,
-      crmv,
-      especialidade
-    );
-    return newVeterinario;
-  },
-};
-export default createVeterinarioService;
+export default class createVeterinarioService {
+  constructor() { }
+  async create(name, email, login, password, especialidade, telefone, crmv) {
+    if (name.length < 5) {
+      return {
+        sucess: false,
+        message: "Nome precisa ter pelo menos 5 caracteres"
+      }
+    }
+    try {
+      const newVeterinario = await VeterinarioModel.create({
+        id: v4(),
+        name,
+        email,
+        login,
+        password,
+        especialidade,
+        telefone,
+        crmv
+
+      })
+      return {
+        sucess: true,
+        message: newVeterinario
+      }
+    } catch(error) {
+      console.log('aqui')
+      console.log(error)
+      return {
+        sucess: true,
+        message: error.message
+      }
+    }
+  }
+}
