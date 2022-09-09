fde8a46ac181ce44f3f50f6b7546dffa96548a81
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-09 14:07:42 -0300

    adiciona chave estrangeira tutor ao metodo de criação do pet

--- src/app/service/PET/CreatePetService.js
+++ src/app/service/PET/CreatePetService.js
@@ -1,24 +1,33 @@
 import petModel from '../../model/Pet';
+import TutorModel from "../../model/TutorModel";
 const { v4 } = require('uuid')
 
 export default class CreatePetService  {
     constructor() {}
 
     async create(
+        tutor,
         nome,
         peso,
         tipoSanguineo,
         raca,
-        idade,
+        idade
+
     ) {
         try {
+
+        const tutorId = TutorModel.findByPk(tutor)
         const newPet = await petModel.create({
             id: v4(),
             nome,
             peso,
             tipoSanguineo,
             raca,
-            idade
+            idade,
+            tutorId
+
+
+
         })
         return newPet;
         } catch (error) {
