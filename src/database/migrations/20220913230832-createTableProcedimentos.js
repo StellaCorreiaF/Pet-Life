dfb80c8e0184c43a69c008a92a95166c5b7019a8
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-13 20:15:06 -0300

    cria migration para procedimentos

---
+++ src/database/migrations/20220913230832-createTableProcedimentos.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+    return queryInterface.createTable('procedimentos', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      tipo: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      data: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      descricao: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      petId: {
+        type: Sequelize.UUID,
+        references: {
+          model: {
+            tableName: 'pets',
+          },
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      vetId: {
+        type: Sequelize.UUID, 
+        references: {
+          model: {
+            tableName: 'veterinarios'
+          }, 
+          key: 'id'
+        }, 
+        allowNull: false
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+   
+    return queryInterface.dropTable('procedimentos')
+  }
+};
