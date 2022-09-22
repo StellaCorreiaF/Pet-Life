44b18c53ab46c579b1e4ed461eccf348fc55f2f7
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-21 22:24:09 -0300

    cria middleware para validar se o pet manipulado pertence ao usuario logado

--- src/middlewares/autorizationValidator.js
+++ src/middlewares/autorizationValidator.js
@@ -1,11 +1,11 @@
-
+import Pets from '../app/models/Pets';
 import type from  '../app/service/AUTH/authModel';
 
 export function tutorIsAuthorized(req, res, next) {
     const user = req.user;
 
     if (user.user_type_id !== type.TutorType)
-        res.status(403).send("Usuário sem permissão");
+        return res.status(403).send("Usuário sem permissão");
 
     next()
 }
@@ -14,9 +14,20 @@ export function VetIsAuthorized(req, res, next) {
     const user = req.user;
 
     if (user.user_type_id !== type.VeterinarioType)
-        res.status(403).send("Apenas veterinários possuem permissão para acessar esta sessão");
+        return res.status(403).send("Apenas veterinários possuem permissão para acessar esta sessão");
 
     next()
 }
 
+export const isTutorOfPet = async (req, res, next) => {
+    const user = req.user;    
+    
+        const petId = req.params.id;
+        const pet = await Pets.findByPk(petId);
+        console.log("pet", pet.tutorId)
+        if (user.id !== pet.tutorId)
+            return res.status(403).send("Usuário não está autorizado a manipular este registro");
+    
+    next()
+}
  
