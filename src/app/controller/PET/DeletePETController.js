7eaa43feb80ae6788c827241ad9ed666b100b817
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-26 20:50:22 -0300

    reformata código

--- src/app/controller/PET/DeletePETController.js
+++ src/app/controller/PET/DeletePETController.js
@@ -1,13 +1,13 @@
-import DeletePetService from "../../service/PET/DeletePetService"; 
+import DeletePetService from "../../service/PET/DeletePetService";
 
-export default class DeletePetController { 
-    constructor(){ 
+export default class DeletePetController {
+    constructor() {
         this.service = new DeletePetService();
-    } 
-    async delete(request, response){ 
-        const {id} = request.params; 
+    }
+    async delete(request, response) {
+        const { id } = request.params;
 
-        const resultado = await this.service.delete(id); 
+        const resultado = await this.service.delete(id);
 
         response.send(resultado)
     }
