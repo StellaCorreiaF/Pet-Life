30c6a597f779368a817032e0da4083029852c1af
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-26 20:50:03 -0300

    transforma endpoint de criar pet para recuperar o tutorId pelo usuario logado

--- src/app/controller/PET/CreatePETController.js
+++ src/app/controller/PET/CreatePETController.js
@@ -1,35 +1,35 @@
 import CreatePetService from "../../service/PET/CreatePetService"
 
 export default class CreatePETController {
-    constructor() {
-      this.service = new CreatePetService();
-    }
-    async create(request, response) {
-      const tutor = request.user; 
-            
-      const {
-          nome,
-          peso,
-          tipoSanguineo,
-          raca,
-          idade,
-          porte,
-          especie        
-      } = request.body;
-      
-      const pet = await this.service.create({
-        nome:  nome,
-        peso: peso,
-        tipoSanguineo: tipoSanguineo,
-        raca: raca,
-        idade: idade,
-        porte: porte,
-        especie: especie,
-        tutorId: tutor.id
+  constructor() {
+    this.service = new CreatePetService();
+  }
+  async create(request, response) {
+    const tutor = request.user;
+
+    const {
+      nome,
+      peso,
+      tipoSanguineo,
+      raca,
+      idade,
+      porte,
+      especie
+    } = request.body;
+
+    const pet = await this.service.create({
+      nome: nome,
+      peso: peso,
+      tipoSanguineo: tipoSanguineo,
+      raca: raca,
+      idade: idade,
+      porte: porte,
+      especie: especie,
+      tutorId: tutor.id
     });
-      if (!pet.sucess) {
-        return response.status(400).json(pet.message);
-      }
-      return response.status(201).json(pet.message)
+    if (!pet.sucess) {
+      return response.status(400).json(pet.message);
     }
+    return response.status(201).json(pet.message)
+  }
 }
