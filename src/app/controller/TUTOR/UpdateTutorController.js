b35cccd7d620260d219461c7806e37b36c6a4d86
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-26 20:56:17 -0300

    reformata código

--- src/app/controller/TUTOR/UpdateTutorController.js
+++ src/app/controller/TUTOR/UpdateTutorController.js
@@ -3,30 +3,28 @@ import UpdateTutorService from "../../service/tutor/UpdateTutorService"
 export default class UpdateTutorController {
   constructor() {
     this.service = new UpdateTutorService();
-  } 
+  }
 
- async update (request, response) {
+  async update(request, response) {
     const { id } = request.params;
-    const { nome, email, username, senha, telefone, cep, bairro, cidade, uf } = request.body;
+    const { nome, email, login, senha, telefone, cep, bairro, cidade, uf } = request.body;
     let user = request.user;
 
     const updatedTutor = await this.service.update(
-         id,
-         nome, 
-         email, 
-         username, 
-         senha, 
-         telefone, 
-         cep, 
-         bairro, 
-         cidade, 
-         uf
+      id,
+      nome,
+      email,
+      login,
+      senha,
+      telefone,
+      cep,
+      bairro,
+      cidade,
+      uf
     );
-    if(user.id !== request.params.id){ 
-      return response.status(403).json({message: "Usuário não está autorizado a alterar "})
- }
-    
-
+    if (!updatedTutor.sucess) {
+      return response.status(400).json(updatedTutor.message);
+    }
     return response.status(200).json(updatedTutor.message);
   }
 }
