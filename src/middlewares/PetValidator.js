5f1fc4c38ac5db912282f3c35749bbf78087a4c3
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-26 20:49:32 -0300

    atualiza middleware do pet

--- src/middlewares/PetValidator.js
+++ src/middlewares/PetValidator.js
@@ -1,12 +1,14 @@
 import * as yup from "yup";
 
-export default async function petValidator (request,response,next) {
+export default async function petValidator(request, response, next) {
     const schema = yup.object().shape({
-        nome:yup.string().strict().required('Nome é obrigatório').typeError('Deve ser uma string'),
+        nome: yup.string().strict().required('Nome é obrigatório').typeError('Deve ser uma string'),
         peso: yup.number().strict().required("Peso é obrigatório").typeError('Deve ser uma número'),
         tipoSanguineo: yup.string().strict().required("Tipo Sanguineo é obrigatório").typeError('Deve ser uma string'),
         raca: yup.string().strict().required("Raça é obrigatório").typeError('Deve ser uma string'),
-        idade: yup.number().strict().required("Idade é obrigatório").typeError('Deve ser uma número'),
+        idade: yup.number().strict().required("Idade é obrigatório").typeError('Deve ser um número'),
+        porte: yup.string().strict().required("Porte é obrigatório, identifique se é seu pet é de pequeno, médio ou grande porte").typeError('Deve ser uma string'),
+        especie: yup.string().strict().required("É necessário identificar a espécie do seu pet").typeError('Deve ser uma string')
     })
 
     await schema.validate(request.body).catch(err => {
