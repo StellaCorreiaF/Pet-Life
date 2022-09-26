e96f56f4d3105d98c2c98c286cacd1c472c14306
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-26 20:51:21 -0300

    adiciona campos

--- src/app/controller/PET/UpdatePETController.js
+++ src/app/controller/PET/UpdatePETController.js
@@ -1,29 +1,35 @@
 import UpdatePetService from "../../service/PET/UpdatePetService";
 
 export default class UpdatePETController {
-    constructor() {
-      this.service = new UpdatePetService();
+  constructor() {
+    this.service = new UpdatePetService();
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      nome,
+      peso,
+      tipoSanguineo,
+      raca,
+      idade,
+      especie,
+      porte
+    } = request.body;
+
+    const updatedPet = await this.service.update(
+      id,
+      nome,
+      peso,
+      tipoSanguineo,
+      raca,
+      idade,
+      porte,
+      especie,
+    )
+    if (!updatedPet.sucess) {
+      return response.status(400).json(updatedPet.message);
     }
-  
-    async update(request, response) {
-      const { id } = request.params;
-      const {
-          nome,
-          peso,
-          tipoSanguineo,
-          raca,
-          idade
-      } = request.body;
-      
-      const updatedPet= await this.service.update(
-          id,
-          nome,
-          peso,
-          tipoSanguineo,
-          raca,
-          idade
-      )
-      
-      return response.status(200).json(updatedPet)     
-    }
-    }
+    return response.status(200).json(updatedPet)
+  }
+}
