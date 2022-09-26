724921785c157f60f578feb6d1b47907c75297f2
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-26 20:52:38 -0300

    altera campos e reformata

--- src/app/controller/TUTOR/CreateTutorController.js
+++ src/app/controller/TUTOR/CreateTutorController.js
@@ -3,22 +3,22 @@ import CreateTutorService from "../../service/TUTOR/CreateTutorService";
 export default class CreateTutorController {
   constructor() {
     this.service = new CreateTutorService();
-  } 
+  }
 
- async create (request, response) {
-    const { id, nome, email, username, senha, telefone, cep, bairro, cidade, uf } = request.body;
+  async create(request, response) {
+    const { id, nome, email, login, senha, telefone, cep, bairro, cidade, uf } = request.body;
 
     const createdTutor = await this.service.create(
-         id,
-         nome, 
-         email, 
-         username, 
-         senha, 
-         telefone, 
-         cep, 
-         bairro, 
-         cidade, 
-         uf
+      id,
+      nome,
+      email,
+      login,
+      senha,
+      telefone,
+      cep,
+      bairro,
+      cidade,
+      uf
     );
 
     if (!createdTutor.sucess) {
