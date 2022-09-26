9ba128ba4bbf74fcfe758193a056cda96b6f7c89
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-26 20:50:33 -0300

    reformata código

--- src/app/controller/PET/ListPETController.js
+++ src/app/controller/PET/ListPETController.js
@@ -1,17 +1,17 @@
-import ListPetService from "../../service/PET/ListPetService"; 
+import ListPetService from "../../service/PET/ListPetService";
 
-export default class ListPetcontroller{ 
-    constructor(){ 
+export default class ListPetcontroller {
+    constructor() {
         this.service = new ListPetService();
-    } 
+    }
 
-    async index(request, response){ 
-        
-        const {name} = request.query; 
+    async index(request, response) {
 
-        const pets = await this.service.listAll(name); 
+        const { name } = request.query;
+
+        const pets = await this.service.listAll(name);
 
         response.json(pets)
     }
-} 
+}
 
