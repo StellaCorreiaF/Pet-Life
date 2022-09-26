4f2d03a027c47b308a1be032bf87be4209cbb48d
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-26 20:57:20 -0300

    reformata

--- src/app/service/AUTH/LoginResult.js
+++ src/app/service/AUTH/LoginResult.js
@@ -2,7 +2,7 @@ export class LoginResult {
     success = false
     token = ""
 
-    constructor(success = false, token = ""){
+    constructor(success = false, token = "") {
         this.success = success
         this.token = token
     }
--- src/app/service/AUTH/LoginService.js
+++ src/app/service/AUTH/LoginService.js
@@ -1,7 +1,7 @@
 import Tutores from "../../models/Tutores";
 import Veterinarios from "../../models/Veterinarios";
 import * as jwt from "jsonwebtoken";
-import type from  './authModel';
+import type from './authModel';
 import { LoginResult } from "./LoginResult";
 import securityConfig from "../../../config/security";
 
@@ -9,33 +9,37 @@ const PRIVATE_KEY = securityConfig.private_key
 
 export default class LoginService {
 
-    async CreateLogin(login, password){
+    async CreateLogin(login, password) {
 
         const tutor = await Tutores.findOne(
-            { where: 
-                {username: login}
+            {
+                where:
+                    { login: login }
             });
-        
-        if (tutor){
-            if (await tutor.verifyPassword(password)){
-                const token = jwt.sign({id: tutor.id, user_type_id: type.TutorType }, PRIVATE_KEY);
+
+        console.log("chegouaqui")
+
+        if (tutor) {
+            if (await tutor.verifyPassword(password)) {
+                console.log("chegou")
+                const token = jwt.sign({ id: tutor.id, user_type_id: type.TutorType }, PRIVATE_KEY);
                 return new LoginResult(true, token)
             }
         }
         else {
             const veterinario = await Veterinarios.findOne(
-                { where: 
-                    {login: login}
+                {
+                    where:
+                        { login: login }
                 });
-            if (veterinario)
-            {
-                if (await veterinario.verifyPassword(password)){
+            if (veterinario) {
+                if (await veterinario.verifyPassword(password)) {
                     console.log("chegou nessa bagaça")
-                    const token = jwt.sign({id: veterinario.id, user_type_id: type.VeterinarioType }, PRIVATE_KEY);
+                    const token = jwt.sign({ id: veterinario.id, user_type_id: type.VeterinarioType }, PRIVATE_KEY);
                     return new LoginResult(true, token);
                 }
             }
-            
+
         }
         return new LoginResult()
     }
